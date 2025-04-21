86edcd72ff447b5049d16420d37eb4ffee747005
james <ueberjames@gmail.com>
2025-04-20 21:47:01 -0300

    navegation

--- src/pages/Agendamento.jsx
+++ src/pages/Agendamento.jsx
@@ -16,7 +16,7 @@ export default function Agendamento() {
     try {
       const appt = await createAgendamento(form);
       alert(`Agendamento  realizado com sucesso!`);
-      navigate('/');
+      
     } catch (err) {
       console.error(err);
       alert(`Erro ao agendar: ${err.message}`);
